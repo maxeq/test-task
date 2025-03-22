93eeffa20be55d56e61218844dd6d175fb336409
maxeq <60312202+maxeq@users.noreply.github.com>
2025-03-22 16:26:58 +0100

    Delete dist directory

--- dist/types/JobTypeMap.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TaskTypes = void 0;
-exports.TaskTypes = {
-    Analysis: "analysis",
-    Notification: "notification",
-    PolygonArea: "polygonArea",
-    ReportGeneration: "reportGeneration",
-};
